f76a3684715879e12e735652f8baf5f2f37e8ce9
Palealmond <trss95@outlook.com>
2023-03-09 15:52:45 -0500

    Added fix for User controller, added more stuff

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -3,7 +3,8 @@ import Post from '../models/posts.js'
 
 export async function getProfile(req, res) {
   const handle = req.params.handle
-  const user = await User.find({ handle })
+  // TODO how to aggregate?
+  const user = await User.findOne({ handle })
   const posts = await Post.find({ author: user._id })
   return res.json({
     ...user.toJSON(),
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,9 +4,9 @@ export default function Post({
   id, text, handle, username,
 }) {
   return (
-    <p key={id}>
-      <h3>{username} <Link to={`/${handle}`}>{handle}</Link></h3>
+    <div key={id}>
+      <h3>{username} <Link to={`/${handle}`}>@{handle}</Link></h3>
       {text}
-    </p>
+    </div>
   )
 }
